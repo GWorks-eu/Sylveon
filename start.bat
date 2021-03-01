2b106e974d1953af3ebfde95e02bc5b68eaffdd5
Hanyaku <68713503+Hanyaku@users.noreply.github.com>
2021-03-01 14:45:03 +0100

    Update start scripts

--- start.bat
+++ start.bat
@@ -1,9 +1,3 @@
-#
-#  Author: Hanyaku
-#  Use: Startup Script for Linux and Mac
-#  Version: 1.0.0
-#  GitHub: https://github.com/Hanyaku/Sylveon/
-#
 sleep 1
 cls
 node Server.js
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #  Author: Hanyaku
 #  Use: Startup Script for Linux and Mac
 #  Version: 1.0.0
-#  GitHub: https://github.com/Hanyaku/Sylveon/
+#  GitHub: https://github.com/GWorks-eu/Sylveon/
 #
 sleep 1
 clear
